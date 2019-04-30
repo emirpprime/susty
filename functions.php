d76bab08e1f0b3ee90c76470c93ac73d6489218c
Phil Banks <phil@helpfuldigital.com>
2019-04-30 12:38:55 +0100

    remove jetpack compatability

--- functions.php
+++ functions.php
@@ -127,13 +127,6 @@ require get_template_directory() . '/inc/template-functions.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 

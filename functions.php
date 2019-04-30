d365e9ecebe4df32432aba1d8529ff270752ea38
Phil Banks <phil@helpfuldigital.com>
2019-04-30 12:46:28 +0100

    re-order and re-work emoji removal
    - move code to near dashicons code
    - make more comprehensive

--- functions.php
+++ functions.php
@@ -127,9 +127,6 @@ require get_template_directory() . '/inc/template-functions.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-
 function susty_nav_rewrite_rule() {
 	add_rewrite_rule( 'menu', 'index.php?menu=true', 'top' );
 }
@@ -149,10 +146,39 @@ add_filter( 'template_include', function( $path ) {
 	return $path;
 });
 
-// Remove dashicons in frontend for unauthenticated users
-add_action( 'wp_enqueue_scripts', 'susty_dequeue_dashicons' );
+
+/**
+ * Remove dashicons in frontend for unauthenticated users
+ */
 function susty_dequeue_dashicons() {
 	if ( ! is_user_logged_in() ) {
 		wp_deregister_style( 'dashicons' );
 	}
+}
+add_action( 'wp_enqueue_scripts', 'susty_dequeue_dashicons' );
+
+/**
+ * Remove emoji extras.
+ * Based on Otto42's: https://gist.github.com/Otto42/b79ff5428993fcff45bb
+ */
+function susty_disable_emoji() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'susty_disable_tinymce_emoji' );
+	add_filter( 'emoji_svg_url', '__return_false' );
+}
+add_action( 'init', 'susty_disable_emoji', 1 );
+/**
+ * Remove the tinymce emoji plugin.
+ *
+ * @param  array $plugins TinyMCE plugins array.
+ * @return array          Modified TinyMCE plugins array.
+ */
+function susty_disable_tinymce_emoji( $plugins ) {
+	return array_diff( $plugins, array( 'wpemoji' ) );
 }

fdb0d1f791859eee1697983c196bb05b2735d4d5
Phil Banks <phil@helpfuldigital.com>
2019-07-15 12:25:39 +0100

    discard Edit link in footer

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -95,22 +95,6 @@ if ( ! function_exists( 'susty_wp_entry_footer' ) ) :
 			echo '</span>';
 		}
 
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'susty' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
 	}
 endif;
 
